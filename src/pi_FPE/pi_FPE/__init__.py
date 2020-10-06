5a06daad9da8950524f60eced133634b4315d815
Nick Kluft <nick.kluft@gmail.com>
2020-10-06 13:41:55 +0200

    Add files via upload

--- src/pi_FPE/pi_FPE/__init__.py
+++ src/pi_FPE/pi_FPE/__init__.py
@@ -2,4 +2,4 @@ from .version import __version__
 from .footplacementestimator import calcFPE
 # if somebody does "from somepackage import *", this is what they will
 # be able to access:
-__all__ = ['clacFPE']
+__all__ = ['calcFPE']
